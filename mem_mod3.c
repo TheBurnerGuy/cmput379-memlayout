3e742a03aac8839e5f3799ffa09eff7d43858189
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-09-27 23:31:08 -0600

    same data allocations done to mem_mod1

--- mem_mod3.c
+++ mem_mod3.c
@@ -15,20 +15,20 @@ int main(){
 		printf("Failed to allocate memory\n");
 		exit(0);
 	}
-	char *place = (char*)mmap(0, 5000000, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0); //place 5 000 000 bytes of anonymous mapping into the memory mapping segment
-	printf("Entering function\n");
-	int memlayout = get_mem_layout(regionlist,35);
-	printf("End: %u\n",memlayout);
-	int i;
-	printf("Printing regions found:\n");
-	char* mode_array[3] = {"RW","RO","NO"};
-	for(i = 0; i<35; ++i){
-		printf("%p-%p %s\n",regionlist[i].from,regionlist[i].to,mode_array[regionlist[i].mode]);
-	}
 	struct memregion* difflist = malloc(sizeof(struct memregion)*10);
 		if (difflist == NULL){
 		printf("Failed to allocate memory\n");
 		exit(0);
+	}
+	int i;
+	char* mode_array[3] = {"RW","RO","NO"};
+	char *place = (char*)mmap(0, 5000000, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0); //place 5 000 000 bytes of anonymous mapping into the memory mapping segment
+	printf("Entering function\n");
+	int memlayout = get_mem_layout(regionlist,35);
+	printf("End: %u\n",memlayout);
+	printf("Printing regions found:\n");
+	for(i = 0; i<35; ++i){
+		printf("%p-%p %s\n",regionlist[i].from,regionlist[i].to,mode_array[regionlist[i].mode]);
 	}
 	
    place = (char*)mmap(0, 5000000, PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0); // change the memory mapping region from read-writeable to read only
