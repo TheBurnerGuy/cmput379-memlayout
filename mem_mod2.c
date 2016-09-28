f4172d78af17c10a9b24b9775ce5b2b041f20f46
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-09-27 23:30:13 -0600

    same data allocations done to mem_mod1

--- mem_mod2.c
+++ mem_mod2.c
@@ -15,19 +15,19 @@ int main(){
 		printf("Failed to allocate memory\n");
 		exit(0);
 	}
-	printf("Entering function\n");
-	int memlayout = get_mem_layout(regionlist,35);
-	printf("End: %u\n",memlayout);
-	int i;
-	printf("Printing regions found:\n");
-	char* mode_array[3] = {"RW","RO","NO"};
-	for(i = 0; i<35; ++i){
-		printf("%p-%p %s\n",regionlist[i].from,regionlist[i].to,mode_array[regionlist[i].mode]);
-	}
 	struct memregion* difflist = malloc(sizeof(struct memregion)*10);
 		if (difflist == NULL){
 		printf("Failed to allocate memory\n");
 		exit(0);
+	}
+	int i;
+	char* mode_array[3] = {"RW","RO","NO"};
+	printf("Entering function\n");
+	int memlayout = get_mem_layout(regionlist,35);
+	printf("End: %u\n",memlayout);
+	printf("Printing regions found:\n");
+	for(i = 0; i<35; ++i){
+		printf("%p-%p %s\n",regionlist[i].from,regionlist[i].to,mode_array[regionlist[i].mode]);
 	}
 	
 	char *place = (char*)mmap(NULL, 500000, PROT_READ | PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0); //place 500 000 bytes of anonymous mapping into the memory mapping segment
