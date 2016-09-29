39768413e7da786a06633f0e11eabee504bf6c9b
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-09-28 23:55:19 -0600

    more quality of life changes

--- mem_mod2.c
+++ mem_mod2.c
@@ -22,9 +22,9 @@ int main(){
 	}
 	int i;
 	char* mode_array[3] = {"RW","RO","NO"};
-	printf("Entering function\n");
+	printf("Entering get_mem_layout\n");
 	int memlayout = get_mem_layout(regionlist,35);
-	printf("End: %u\n",memlayout);
+	printf("# of regions found: %u\n",memlayout);
 	printf("Printing regions found:\n");
 	for(i = 0; i<35; ++i){
 		printf("%p-%p %s\n",regionlist[i].from,regionlist[i].to,mode_array[regionlist[i].mode]);
@@ -37,44 +37,12 @@ int main(){
 		exit(0);
 	}
 	
+	printf("Entering get_diff_layout\n");
 	memlayout = get_mem_diff(regionlist, 35, difflist, 10);
-	printf("End: %u\n",memlayout);
+	printf("# of different regions found: %u\n",memlayout);
 	printf("Printing difference found:\n");
 	for(i = 0; i<10; ++i){
 		printf("%p-%p %s\n",difflist[i].from,difflist[i].to,mode_array[difflist[i].mode]);
 	}
 }
 
-//jmp_buf env;
-
-//Test Function
-//~ void signal_handler(int sig){
-	//~ printf("I received SEGFAULT!\n");
-	//~ siglongjmp(env, 1);
-//~ }
-
-//int main(){
-	
-	//~ jmp_buf env;
-	//~ int test = sigsetjmp(env,1);
-	//~ 
-	//~ void signal_handler(int sig){
-		//~ siglongjmp(env, 1);
-	//~ }
-	//~ 
-	//~ struct sigaction segv;
-	//~ segv.sa_handler = signal_handler;
-	//~ sigemptyset(&segv.sa_mask);
-	//~ segv.sa_flags = 0;
-	//~ sigaction(SIGSEGV,&segv,0);
-	//~ 
-	//~ unsigned int* testpointer = (unsigned int*)0xFFFF;
-	//~ if(!test) printf("Printing: %d\n",*testpointer);
-	//~ printf("Cannot be written, reading\n");
-	//~ 
-	//~ test = sigsetjmp(env,1);
-	//~ if(!test) *testpointer = 1;
-	//~ printf("Done\n");
-	
-	//return 0;
-//}
